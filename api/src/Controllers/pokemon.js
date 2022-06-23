de7be0133d06d4ef21ca5fa3b3c55eacd718d543
enriquegomeznaar <enriquegomeznaar@gmail.com>
2022-06-23 14:07:27 -0300

    comitteo

--- api/src/Controllers/pokemon.js
+++ api/src/Controllers/pokemon.js
@@ -1,10 +1,11 @@
 const axios = require("axios");
 const { Pokemon, Types } = require("../db");
 const API_URL_POKES = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10";
+const { v4: uuidv4 } = require("uuid");
 const { Router } = require("express");
 const router = Router();
 
-// 1. Get pokemon (imagen, nombre, tipo)
+//  Get pokemon (imagen, nombre, tipo)
 
 async function getAllApi() {
   const getApiInfo = await axios.get(`${API_URL_POKES}`);
@@ -35,7 +36,6 @@ async function getAllApi() {
   return pokesDetail;
 }
 
-
 async function getAllDB() {
   return await Pokemon.findAll({
     include: {
@@ -55,7 +55,6 @@ async function getAllPokemons() {
   //console.log(apiDB)
 }
 
-
 //  Get pokemon por nombre (query).
 
 async function getPokemonByName(req, res, next) {
@@ -68,25 +67,37 @@ async function getPokemonByName(req, res, next) {
     pokemonsName.length
       ? res.status(200).send(pokemonsName)
       : res.status(404).send("No se encontro el Pokemon ingresado...");
-  } else{
-    res.status(200).send(pokemonsTotal)
+  } else {
+    res.status(200).send(pokemonsTotal);
   }
   // console.log(name)
 }
 
-// Obtener todos los tipos de pokemons posibles
-async function getAllToBD(){
-  try {
-    let types = (await axios.get("https://pokeapi.co/api/v2/type")).data.results.map(p=>({
-      name: p.name
-    }))
-    await Types.bulkCreate(types)
-    console.log("Tipos cargados en BD correctamente")
-  } catch (error) {
-      console.log(error)
+// Crear un pokemon.
+async function postPokemon(req, res, next) {
+  
+    const { name, ph, strength, defense, speed, height, weight, image, createdInDb, type } =
+      req.body;
+
+    let pokemonCreated = await Pokemon.create ({
+      id: uuidv4(),
+      name:name,
+      ph:ph,
+      strength:strength,
+      defense:defense,
+      speed:speed,
+      height:height,
+      weight:weight,
+      image:image,
+      createdInDb:createdInDb
+    });
+    // console.log(pokemonCreated)
+    const typeBD = await Types.findAll({
+      where: { name: type },
+    });
+    pokemonCreated.addType(typeBD);
+    res.send("Pokemon creado con exito!");
   }
-  console.log(types);
-}
 
 // 2. Get pokemon por id ( .Los campos mostrados en la ruta principal para cada pokemon (imagen, nombre y tipos)
 //                      .Número de Pokemon (id)
@@ -99,4 +110,5 @@ async function getAllToBD(){
 module.exports = {
   getAllPokemons,
   getPokemonByName,
+  postPokemon,
 };
--- api/src/routes/pokemon.js
+++ api/src/routes/pokemon.js
@@ -1,10 +1,14 @@
 const { Router } = require("express");
-const { getAllPokemons, getPokemonByName } = require("../Controllers/pokemon");
+const {
+  getAllPokemons,
+  getPokemonByName,
+  postPokemon,
+} = require("../Controllers/pokemon");
 
 const router = Router();
 
 router.get("/", getAllPokemons);
 router.get("/name", getPokemonByName);
-
+router.post("/", postPokemon);
 
 module.exports = router;
